64691daaf3df976b5175cf1860e113f7d125c704
ColeMcGregor <gc28mcgr@siena.edu>
2025-05-31 20:19:15 -0400

    added in an incomplete but relatively filled out Player class, a GameObject subclass. will not some fine tuning for values and some stubs filled out

---
+++ SkyDiver/app/src/main/java/com/colemcg/skydiver/core/entities/Player.kt
@@ -0,0 +1,75 @@
+package com.colemcg.skydiver.core.entities
+
+import com.colemcg.skydiver.core.geometry.Vector2
+import com.colemcg.skydiver.core.geometry.Rect
+import com.colemcg.skydiver.platform.GameRenderer
+import com.colemcg.skydiver.core.entities.GameObject
+import com.colemcg.skydiver.core.entities.Collectible
+import com.colemcg.skydiver.core.entities.Obstacle
+
+//player state enum for switch cases
+enum class PlayerState {
+    Normal, Slowed, Dead
+}
+
+//player class
+//is a subclass of GameObject, and therefore has a position, velocity, and hitbox
+class Player(
+    position: Vector2,
+    velocity: Vector2 = Vector2(0f, 0f)
+) : GameObject(position, velocity) {
+
+    //player state at start
+    var state: PlayerState = PlayerState.Normal
+
+    //player current speed
+    var currentSpeed: Float = 1.0f
+
+    override val hitbox: Rect = Rect(position.x, position.y, width = 30f, height = 50f)
+
+    //increase speed
+    fun goFaster() { currentSpeed += 0.1f }
+
+    //increase speed by amount
+    fun goFaster(amount: Float) { currentSpeed += amount }
+
+    //decrease speed
+    fun goSlower() { currentSpeed -= 0.1f }
+
+    //decrease speed by amount
+    fun goSlower(amount: Float) { currentSpeed -= amount }
+
+
+    /*The below are still stub functions, and will be implemented in the future */
+
+    //move left/right  with slide input
+    fun slide(input: InputEvent) { 
+        // move left/right  with slide input
+    }
+
+    //collect collectible, used for collectible logic
+    fun onCollect(collectible: Collectible) {
+        // Implement collectible logic here
+    }
+
+    //collision with obstacle, this is a typical name for a function that handles collisions
+    fun onCollision(obstacle: Obstacle) {
+        // Implement collision logic here
+    }
+
+//override functions from GameObject
+
+    //update player position
+    //this is a classic update pattern for a game object, used to allow for smooth movement/actions to occur
+    //it helps simulate time passing, and allows for smooth animations
+    override fun update(deltaTime: Float) {
+        position += velocity * deltaTime
+        hitbox.x = position.x
+        hitbox.y = position.y
+    }
+
+    //draw player
+    override fun onDraw(renderer: GameRenderer) {
+        renderer.drawPlayer(position)
+    }
+}
