6a2c7feb35e361fad5a4c8c8eb6481972526261a
ColeMcGregor <gc28mcgr@siena.edu>
2025-05-31 20:01:51 -0400

    created level abstract class. basic at this point. cant think of other things it might need though

---
+++ SkyDiver/app/src/main/java/com/colemcg/skydiver/core/levels/level.kt
@@ -0,0 +1,27 @@
+package com.colemcg.skydiver.core.levels
+
+import com.colemcg.skydiver.core.entities.GameObject
+import com.colemcg.skydiver.core.entities.Obstacle
+import com.colemcg.skydiver.core.entities.Collectible
+
+abstract class Level {
+
+    abstract val name: String                    // e.g., "Volcano Tube"
+    abstract val backgroundImage: String         // PNG or resource ID
+    open val initialOffset: Float = 0f           // Optional vertical scroll start
+
+    /**
+     * Called once to populate background visuals (e.g., clouds, volcano jets)
+     */
+    abstract fun createBackgroundObjects(): List<GameObject>
+
+    /**
+     * Called when difficulty manager decides it's time to spawn an obstacle
+     */
+    abstract fun generateObstacle(): Obstacle?
+
+    /**
+     * Called when a collectible should appear
+     */
+    abstract fun generateCollectible(): Collectible?
+}
