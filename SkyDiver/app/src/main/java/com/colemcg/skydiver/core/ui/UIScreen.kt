715fa7e3533a715e5ed71680e9854c22425aef06
ColeMcGregor <gc28mcgr@siena.edu>
2025-05-31 20:50:33 -0400

    added the UIScreen Interface to be turned into things like StartScreen and OptionsMenu etc

---
+++ SkyDiver/app/src/main/java/com/colemcg/skydiver/core/ui/UIScreen.kt
@@ -0,0 +1,32 @@
+package com.colemcg.skydiver.core.ui
+
+import com.colemcg.skydiver.core.events.InputEvent
+
+/**
+ * Interface for UI screens such as the start screen, options menu, or game over overlay.
+ * Each screen is responsible for drawing itself and handling input events.
+ * 
+ * @author Cole McGregor
+ */
+interface UIScreen {
+
+    /**
+     * Called when the screen becomes visible.
+     * Initialize animations or reset internal state here.
+     */
+    fun show()
+
+    /**
+     * Called when the screen is hidden.
+     * Cleanup or pause activities here if necessary.
+     */
+    fun hide()
+
+    /**
+     * Called when an input event (e.g., tap, drag) is dispatched to this screen.
+     * Use this to handle button presses, menu navigation, etc.
+     *
+     * @param event the input event to respond to
+     */
+    fun handleInput(event: InputEvent)
+}
