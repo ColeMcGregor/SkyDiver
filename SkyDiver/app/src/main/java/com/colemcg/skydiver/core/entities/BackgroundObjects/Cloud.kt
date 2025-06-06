73bfee085fe08c3a50409de61929c5c106290c2d
ColeMcGregor <gc28mcgr@siena.edu>
2025-06-05 20:46:51 -0400

    feat,documentation(entities): added in stub file with top comment for Cloud background object

---
+++ SkyDiver/app/src/main/java/com/colemcg/skydiver/core/entities/BackgroundObjects/Cloud.kt
@@ -0,0 +1,16 @@
+/**
+ * Passive background object that drifts across the screen.
+ *
+ * Clouds provide visual depth and motion to the game world
+ * without affecting gameplay. They use a parallax factor
+ * to create the illusion of layered backgrounds.
+ *
+ * Continuously loops when moving off-screen to maintain
+ * environmental continuity.
+ *
+ * Inherits from BackgroundObject and participates in the draw
+ * and update loop, even during paused or game over states.
+ *
+ * @see BackgroundObject
+ * @author 
+ */
