461762253750084ce36a32fc4c1bcc8e56e92301
ColeMcGregor <gc28mcgr@siena.edu>
2025-08-07 11:21:57 -0400

    Feat(Coin): Implemented Coin, including relevant aspects of motion special to the coin. onCollect is not yet implemented. updated top comments

--- SkyDiver/app/src/main/java/com/colemcg/skydiver/core/entities/Collectibles/Coin.kt
+++ SkyDiver/app/src/main/java/com/colemcg/skydiver/core/entities/Collectibles/Coin.kt
@@ -1,3 +1,11 @@
+package com.colemcg.skydiver.core.entities.Collectibles
+
+import com.colemcg.skydiver.core.geometry.Vector2
+import kotlin.math.sin
+import kotlin.math.cos
+import com.colemcg.skydiver.core.entities.Collectible
+import com.colemcg.skydiver.core.entities.Player
+
 /**
  * A simple collectible that increases the player's score.
  *
@@ -6,9 +14,38 @@
  *
  * It inherits from the base Collectible class and defines
  * its own point value.
+ * 
+ * moves in a circular motion, and drifts upward
  *
  * Safe to spawn frequently to encourage player movement.
  *
  * @see Collectible
- * @author 
+ * @author Cole McGregor
  */
+
+class Coin(position: Vector2 = Vector2()) : Collectible(position) {
+
+    override val spriteName = "coin"
+    override val spriteSize = Vector2(32f, 32f)
+
+    private var time = 0f
+    private val angularSpeed = 2f         // radians per second
+    private val radius = 40f              // horizontal circle radius
+    private val upwardSpeed = -40f        // upward drift speed (pixels/second)
+
+    override fun onCollect(player: Player) {
+        // TODO: Implement coin collection logic
+    }
+
+    override fun update(deltaTime: Float) {
+        time += deltaTime
+        val angle = time * angularSpeed
+
+        // Circular horizontal offset
+        val dx = cos(angle) * radius * deltaTime
+        val dy = upwardSpeed * deltaTime
+
+        position.x += dx
+        position.y += dy
+    }
+}
