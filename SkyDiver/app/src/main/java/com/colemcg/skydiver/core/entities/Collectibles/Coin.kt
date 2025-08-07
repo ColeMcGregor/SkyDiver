00bf9663683c2598dda7dc2fa5891318ee9db128
ColeMcGregor <gc28mcgr@siena.edu>
2025-08-07 12:14:19 -0400

    Feat(Coin): added in first version of onCollect, will need tuning

--- SkyDiver/app/src/main/java/com/colemcg/skydiver/core/entities/Collectibles/Coin.kt
+++ SkyDiver/app/src/main/java/com/colemcg/skydiver/core/entities/Collectibles/Coin.kt
@@ -5,6 +5,8 @@ import kotlin.math.sin
 import kotlin.math.cos
 import com.colemcg.skydiver.core.entities.Collectible
 import com.colemcg.skydiver.core.entities.Player
+import com.colemcg.skydiver.core.systems.ScoreManager
+import com.colemcg.skydiver.core.systems.SoundManager
 
 /**
  * A simple collectible that increases the player's score.
@@ -23,20 +25,35 @@ import com.colemcg.skydiver.core.entities.Player
  * @author Cole McGregor
  */
 
+//const for coin point value
+const val COIN_POINT_VALUE = 100
+
 class Coin(position: Vector2 = Vector2()) : Collectible(position) {
 
     override val spriteName = "coin"
-    override val spriteSize = Vector2(32f, 32f)
+    override val spriteSize = Vector2(6f, 10f)
 
     private var time = 0f
     private val angularSpeed = 2f         // radians per second
     private val radius = 40f              // horizontal circle radius
     private val upwardSpeed = -40f        // upward drift speed (pixels/second)
 
-    override fun onCollect(player: Player) {
-        // TODO: Implement coin collection logic
+    override fun onCollect(player: Player, scoreManager: ScoreManager, soundManager: SoundManager) {
+        println("Coin collected")
+        //add points to the score manager
+        scoreManager.addPoints(COIN_POINT_VALUE)
+        //increment the number of coins collected by the player this run
+        scoreManager.incrementCoinsCollected()
+        //play the coin sound
+        soundManager.playSFX("coin")
     }
 
+    /*
+    * Updates the coin's position based on its position, and the elapsed time.
+    * DOESNT USE VELOCITY, as it is a circular motion, and drifts upward
+    * @param deltaTime The time passed since the last update (in seconds).
+    * @author Cole McGregor
+    */
     override fun update(deltaTime: Float) {
         time += deltaTime
         val angle = time * angularSpeed
