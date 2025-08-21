95ad5f4912d95ca0dbb0c77c5cd24732a1478918
JardinaG <ja19gome@siena.edu>
2025-08-21 13:04:10 -0400

    feat(entities): added initial CLoud class implementaion, introducing Cloud as subclass, provided sprite metadata for renderer

--- SkyDiver/app/src/main/java/com/colemcg/skydiver/core/entities/BackgroundObjects/Cloud.kt
+++ SkyDiver/app/src/main/java/com/colemcg/skydiver/core/entities/BackgroundObjects/Cloud.kt
@@ -1,3 +1,7 @@
+import com.colemcg.skydiver.core.entities.BackgroundObject
+import com.colemcg.skydiver.core.geometry.Rect
+import com.colemcg.skydiver.core.geometry.Vector2
+
 /**
  * Passive background object that drifts across the screen.
  *
@@ -12,5 +16,35 @@
  * and update loop, even during paused or game over states.
  *
  * @see BackgroundObject
- * @author 
+ * @author Jardina Gomez
  */
+
+class Cloud(
+    startPos: Vector2, // initial position of the cloud in the game world
+    // width and height of the cloud sprite in pixels
+    private val width: Float,
+    private val height: Float,
+    // base drift speed in Y direction, pixels/sec (positive = down)
+    private val speedY: Float,
+    // adding this but not sure if it will be used yet, for horizontal drift to simulate some wind
+    private val windX: Float = 0f,
+    // parallax depth: .3f(far) - 1.0f(near)
+    override val parallaxFactor: Float,
+    private val getLoopHeight: () -> Float // function to get the height of the background loop
+
+    ): BackgroundObject(
+        position = startPos.copy(), // copy to avoid mutating the original Vector2
+        velocity = Vector2(windX, speedY),
+        parallaxFactor = parallaxFactor
+
+    ){
+        /** Sprite metadata used by renderer*/
+        override val spriteName = "cloud"
+        override val spriteSize = Vector2(width, height)
+
+        /** computed hitbox from current position & sprite size so it stays in sync */
+        override val hitbox: Rect
+            get() = Rect(position.x, position.y, spriteSize.x, spriteSize.y)
+
+
+    }
